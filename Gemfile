8b45995ebea44150d167d9e9e7ff267a5c51f9ac
nevergiveup2212 <blackcat22121996@gmail.com>
2017-12-11 09:04:37 +0700

    a

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+# get location
+gem 'geocoder'
 gem 'devise'
 gem 'cancancan'
 gem 'bootstrap-sass'
--- app/models/user.rb
+++ app/models/user.rb
@@ -51,4 +51,15 @@ end
     end
     user
 end
+
+geocoded_by :full_address # full_address is a method which take some model's attributes to get a formatted address for example
+
+  # the callback to set longitude and latitude
+  after_validation :geocode
+
+  # the full_address method
+  def full_address
+    "#{address}, #{zipcode}, #{city}, #{country}"
+  end
+
 end
