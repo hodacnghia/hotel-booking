e3a0a81a08caca49853e0c3e98d0e814bb980760
nghia <hdn.hodacnghia.hdn@gmail.com>
2017-12-27 11:57:40 +0700

    show orders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from CanCan::AccessDenied do |exception|
     flash[:error] = "Access denied!"
+    redirect_to main_app.root_url, :alert => exception.message
+    
   end
 
   protected
