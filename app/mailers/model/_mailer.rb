16ff5530df0942b3dc0bdf7ac4470099370843ec
Nghia Ho <hdn.hodacnghia.hdn@gmail.com>
2018-01-10 08:27:15 +0700

    update mail

--- app/mailers/model/_mailer.rb
+++
@@ -1,7 +0,0 @@
-class _mailer < ApplicationMailer
-  default from: "me@MYDOMAIN.com"
-  def new_record_notification(record)
-  @record = record
-  mail to: "recipient@MYDOMAIN.com", subject: "Success! You did it."
-end
-end
