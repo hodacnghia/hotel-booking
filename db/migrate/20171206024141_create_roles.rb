9e9b0d529fae1690da434332ff67218382bc6782
Nghia Ho <hdn.hodacnghia.hdn@gmail.com>
2018-01-10 09:29:16 +0700

    update db

--- db/migrate/20171206024141_create_roles.rb
+++ db/migrate/20171206024141_create_roles.rb
@@ -1,8 +1,8 @@
 class CreateRoles < ActiveRecord::Migration[5.1]
   def change
     create_table :roles do |t|
-      t.string :name
-      t.string :description
+      t.string :name_role
+      t.string :description_role
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -134,8 +134,8 @@ ActiveRecord::Schema.define(version: 20180102053631) do
   end
 
   create_table "roles", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
+    t.string "name_role"
+    t.string "description_role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
