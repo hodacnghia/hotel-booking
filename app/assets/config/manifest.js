25d67d2fa079ac37ada65bf61026e004ba9d6233
Nghia Ho <hdn.hodacnghia.hdn@gmail.com>
2018-01-10 22:53:11 +0700

    manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,6 @@
+//= link application.js
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//= link_directory ../stylesheets .scss
+//= link_tree ../javascripts .js
 //= link_tree ../images
-//= link_directory ../application.js
-//= link_directory ../stylesheets.css
