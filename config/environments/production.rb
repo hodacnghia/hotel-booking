c08193d5e7856e769f8a7645c395d115487f3646
Nghia Ho <hdn.hodacnghia.hdn@gmail.com>
2018-01-10 16:56:19 +0700

    fix bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
   config.cache_classes = true
   config.assets.compile = true
   config.assets.digest = true
+  config.serve_static_assets = true
+
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
